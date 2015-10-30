0a4b1f343b2c94d3896d403a58bacd165027b958
Miles Essmiller <miles@noesclavo.com>
2015-10-30 11:27:57 -0700

    glow

---
+++ y_glow/y_glow.2.glsl
@@ -0,0 +1,15 @@
+#version 120
+
+uniform sampler2D Matte, adsk_results_pass1;
+uniform float adsk_result_w, adsk_result_h;
+vec2 res = vec2(adsk_result_w, adsk_result_h);
+vec2 texel  = vec2(1.0) / res;
+
+void main(void)
+{
+	vec2 st = gl_FragCoord.xy / res;
+	vec3 front = texture2D(adsk_results_pass1, st).rgb;
+	float matte = texture2D(Matte, st).r;
+
+	gl_FragColor = vec4(front, matte);
+}
