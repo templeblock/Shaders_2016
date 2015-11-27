a7f5417ba46b05fa86fe2fe0c4379c3505f0b920
Miles Essmiller <miles@noesclavo.com>
2015-11-27 10:34:45 -0800

    trouble shoot webhhok 17

---
+++ y_mult_div/y_mult_div.1.glsl
@@ -0,0 +1,34 @@
+#version 120
+
+#define ratio adsk_result_frameratio
+
+uniform float ratio;
+uniform float adsk_result_w, adsk_result_h;
+vec2 res = vec2(adsk_result_w, adsk_result_h);
+vec2 texel = vec2(1.0) / res;
+
+uniform sampler2D Front;
+uniform sampler2D Matte;
+
+uniform bool divide;
+uniform bool invert;
+
+void main(void) {
+	vec2 st = gl_FragCoord.xy / res;
+
+	vec3 front = texture2D(Front, st).rgb;
+	float matte = texture2D(Matte, st).r;
+
+
+	if (invert) {
+		matte = 1.0 - matte;
+	}
+
+	if (divide) {
+		front = front / max(matte, .00001);
+	} else {
+		front *= matte;
+	}
+
+	gl_FragColor = vec4(front, matte);
+}
